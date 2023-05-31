e81fcdd33cdaafb7d17a59d8ce64676b585bf7c4
Kaua J <kauajuno@gmail.com>
2023-05-31 13:42:31 -0300

    [GI] Inheritance + generics refactoring

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,7 +1,14 @@
-export class View{
+export abstract class View<T>{
     protected _div : HTMLElement;
 
     constructor(selector : string){
         this._div = document.querySelector(selector);
+    }
+
+    abstract template(model : T) : string;
+
+    update(model : T) : void{
+        const template = this.template(model);
+        this._div.innerHTML = template;
     }
 }
--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -2,4 +2,8 @@ export class View {
     constructor(selector) {
         this._div = document.querySelector(selector);
     }
+    update(model) {
+        const template = this.template(model);
+        this._div.innerHTML = template;
+    }
 }
